259385b2aca1cdd2cd838cfa625193250467793b
442774765 <74840658+442774765@users.noreply.github.com>
2021-09-17 16:30:07 +0800

    Add files via upload

--- v4_refreshCK.py
+++ v4_refreshCK.py
@@ -14,14 +14,13 @@ def getconfig():
 
 def wstopt(wskey):
     try:
-        url = "http://cdn.xia.me/getck"
+        url = "https://signer.nz.lu/getck"
         headers = {
-            "Host": "signer.nz.lu",
             "user-agent": "Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
         }
         data = {"wskey": wskey, "key": "xb3z4z2m3n847"}
         r = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
-        return r.text
+        return r
     except:
         return "error"
 
@@ -32,16 +31,26 @@ if __name__ == '__main__':
         pin = re.findall(r"pt_(pin=.*?);", i)[0]
         for ii in wskeys:
             if pin in ii:
-                ptck = wstopt(ii)
-                if ptck == "wskey错误":
-                    print("有一个wskey可能过期了,%s" % pin)
-                elif ptck == "未知错误" or ptck == "error":
-                    print("有一个wskey发生了未知错误,%s" % pin)
-                else:
-                    with open('/jd/config/config.sh', '+r') as f:
-                        t = f.read()
-                        t = t.replace(i, ptck)
-                        f.seek(0, 0)
-                        f.write(t)
-                        f.truncate()
-                    print(pin + "更新成功！")
+                try:
+                    r = wstopt(ii)
+                    ptck = r.text
+                    if r.status_code == 429:
+                        print("您的ip请求api过于频繁，已被流控")
+                        exit()
+                    else:
+                        if ptck == "wskey错误":
+                            print("有一个wskey可能过期了,%s" % pin)
+                        elif ptck == "未知错误" or ptck == "error":
+                            print("有一个wskey发生了未知错误,%s" % pin)
+                        elif "</html>" in ptck:
+                            print("你的ip被cloudflare拦截")
+                        else:
+                            with open('/jd/config/config.sh', '+r') as f:
+                                t = f.read()
+                                t = t.replace(i, ptck)
+                                f.seek(0, 0)
+                                f.write(t)
+                                f.truncate()
+                            print(pin + "更新成功！")
+                except:
+                    print("发生了未知错误")
